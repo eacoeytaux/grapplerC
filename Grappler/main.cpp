48e5707b8e89064b916f79467947e21c68b559a9
eacoeytaux <eacoeytaux@gmail.com>
2015-08-23 12:51:09 -0400

    Initial Commit

---
+++ Grappler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Grappler
+//
+//  Created by Ethan Coeytaux on 8/23/15.
+//  Copyright (c) 2015 Firework Gamelabs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
